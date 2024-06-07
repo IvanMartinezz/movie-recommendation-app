e0c5f6c4465b571258de2c8c5d53ed39ebb02f2a
Ivan Martinez <50034274+IvanMartinezz@users.noreply.github.com>
2024-06-07 13:11:10 -0300

    se creo la ui del proyecto
    Update page.tsx

---
+++ README.md
@@ -0,0 +1,2 @@
+# movie-recommendation-app
+Creación de aplicación de recomendación de películas con renderizado del lado del servidor
---
+++ ui/src/app/page.tsx
@@ -0,0 +1,17 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+        <div></div>
+      </div>
+
+      <div className={styles.center}></div>
+    </main>
+  );
+}
